dd9c83362ab0d43f5d307c54b20c9beab2b3c19e
miatey <troyan@ukr.net>
2016-09-16 17:14:21 +0300

    +

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
 
-	// go js
+	$('.btn-nav').on('click', function(){
+			var menu = $('.header__nav');
+			menu.toggleClass('open-nav')
+		});
+
+	$('.js-slick').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 2000,
+	});
 
 });
